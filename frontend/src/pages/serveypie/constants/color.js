8a8e899e4ce27a7f29df74399bbee1742390b830
heojungyoung <tyireal@gmail.com>
2025-05-31 18:35:07 +0900

    commit test

---
+++ frontend/src/pages/serveypie/constants/color.js
@@ -0,0 +1,76 @@
+export const PRIMARY = {
+
+  BUTTON : {
+      DEFAULT : {
+          COLOR : '#FFFFFF',
+          BACKGROUND : '#6542F1'
+      },
+      HOVER : {
+          COLOR : '#FFFFFF',
+          BACKGROUND : '#9982F4'
+      },
+      PRESSED : {
+          COLOR : '#FFFFFF',
+          BACKGROUND : '#4B31B0'
+      },
+      DISABLED : {
+          COLOR : '#D0CDCD',
+          BACKGROUND : '#EDEDED'
+      }
+  }
+
+}
+
+export const SECONDARY = {
+
+    BUTTON : {
+        DEFAULT : {
+            COLOR : '#DEDEDE',
+            BACKGROUND : '#DEDEDE'
+        },
+        HOVER : {
+            COLOR : '#F3F3F3',
+            BACKGROUND : '#F3F3F3'
+        },
+        PRESSED : {
+            COLOR : '#B8B7B9',
+            BACKGROUND : '#B8B7B9'
+        },
+        DISABLED : {
+            COLOR : '#D0CDCD',
+            BACKGROUND : '#EDEDED'
+        }
+    }
+
+}
+
+
+export const TERTIARY = {
+
+    BUTTON : {
+        DEFAULT : {
+            COLOR : '#FFFFFF',
+            BACKGROUND : '#6542F1',
+            BORDER : '#6542F1'
+        },
+        HOVER : {
+            COLOR : '#9982F4',
+            BACKGROUND : '#FFFFFF',
+            BORDER : '#9982F4'
+        },
+        PRESSED : {
+            COLOR : '#4B31B0',
+            BACKGROUND : '#F0EDFC',
+            BORDER : '#4B31B0'
+        },
+        DISABLED : {
+            COLOR : '#D0CDCD',
+            BACKGROUND : '#F6F6F6',
+            BORDER : '#EDEDED'
+        }
+    }
+
+}
+
+
+
